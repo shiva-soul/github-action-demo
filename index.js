0a70b31f9372e055e0abbd5b674d48a0b9b967d7
shiva-soul <shivanandmishra14@gmail.com>
2024-08-19 21:37:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// index.js
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = sum;
+
---
+++ sum.test.js
@@ -0,0 +1,7 @@
+// sum.test.js
+const sum = require('./index');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
